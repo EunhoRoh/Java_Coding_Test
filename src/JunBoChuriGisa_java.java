235e3e34ecff78fc967dc9958d1ea781cd21d0d7
EunhoRoh <deu04189@gmail.com>
2023-07-19 23:37:43 +0900

    BJ 16967 배열 복원하기

---
+++ src/JunBoChuriGisa_java.java
@@ -0,0 +1,9 @@
+public class JunBoChuriGisa_java {
+    public static void main(String[] args){
+        String s="red";
+        boolean[]b = new boolean[1];
+        if(b[0])
+            s="blue";
+        System.out.print(s);
+    }
+}
